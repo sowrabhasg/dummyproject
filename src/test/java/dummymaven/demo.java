39136e950e33c10d1a203dfc843f47ae8de06bc9
sowrabhasg <147090377+sowrabhasg@users.noreply.github.com>
2024-08-14 09:02:19 +0530

    Update demo.java

--- src/test/java/dummymaven/demo.java
+++ src/test/java/dummymaven/demo.java
@@ -2,9 +2,10 @@ package dummymaven;
 
 public class demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		
+		system.output.println("sowrabha");
 	}
 
 }
